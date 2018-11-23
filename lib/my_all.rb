79baffade00dedd29637404ceb093e7da083a346
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 03:52:25 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,5 @@ def my_all?(collection)
     block_return_values.push(yield(collection[i]))
     counter += 1
   end
-  block_return_values.any? {|element| element == false} ? false : true
+  block_return_values.include?(false) ? false : true
 end
