c76aff64f67d685581125ed47d9483bd409fe283
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 03:48:51 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  block_return_values = []
+  while conter < collection.length
+    block_return_values.push(yield(collection[i]))
+    counter += 1
+  end
 end
